9b744e3b66f26086959583ce1ab2435b56907297
Ritesh <riteshyadav825410@gmail.com>
2026-04-16 17:44:30 +0530

    working on left and right components

--- src/components/Left/Left.jsx
+++ src/components/Left/Left.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+// ✅ Correct
+import { MoveUpRight } from "lucide-react";
 
 const Left = () => {
   return (
     <div className="bg-red-400 h-screen w-25/70">
-      <h1>My name is RITESH </h1>
+        <div className="m-5 mt-20">
+            <h1 className="font-bold text-[35px] leading-[34px]">Prospective<br />Customer<br />Segment</h1>
+            <p className="text-[16px] leading-[24px] mt-7">Lorem, ipsum dolor sit amnderit temporibus ipsa sed, <br/>placeat natus rerum! Veritatis quae,<br/> libero doloremque vel esse eum nisi iste ea.</p>
+             <MoveUpRight className="mt-30" />
+        </div>
     </div>
   )
 }
 
-export default Left
+export default Left;
--- src/components/Right/Right.jsx
+++ src/components/Right/Right.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
 
 const Right = () => {
   return (
     <div className="bg-[green] h-screen w-45/70 float-right">
-      <h1>My name is RITESH </h1>
+      <div className="relative h-[67%] w-1/3 bg-[blue] m-5 mt-20">
+        <img
+          className="w-full h-full object-cover"
+          src="https://i.pinimg.com/1200x/b9/0a/21/b90a21804d5cca6999e55a4739ab70bb.jpg"
+        />
+        <div className="absolute bottom-0 left-0 right-0 bg-transparent bg-opacity-70 text-white p-4">
+            <div>1</div>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ducimus voluptatum aperiam dolore culpa rerum totam atque.</div>
+            <button className="bg-black text-white p-2 rounded-[20px] mt-5">READ MORE</button>
+            <button>ARROW</button>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Right
+export default Right;
